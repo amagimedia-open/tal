45614689baf111e7494a358985360968d5908967
ksridhar <ksridhar@amagi.com>
2021-05-17 20:12:18 +0530

    added more examples

--- simpy-examples/20_topicenv_startdelay.py
+++ simpy-examples/20_topicenv_startdelay.py
@@ -1,11 +1,13 @@
 import simpy
+from simpy.util import start_delayed
 
 def sub(env):
     yield env.timeout(1)
     return 23
 
 def parent(env):
-    ret = yield env.process(sub(env))
+    sub_proc = yield start_delayed(env, sub(env), delay=3)
+    ret = yield sub_proc
     return ret
 
 env = simpy.Environment()
@@ -14,5 +16,8 @@ ret_val = env.run(env.process(parent(env)))
 
 assert ret_val == 23
 
+assert env.now == 4
+
 print(ret_val)
 print(env.now)
+
