2140585022a525f5209004bc361da4796926d134
ksridhar <ksridhar@amagi.com>
2021-05-21 19:59:03 +0530

    some refactoring in trials/blip-player/05/run.py

--- trials/blip-player/05/run.py
+++ trials/blip-player/05/run.py
@@ -1,39 +1,66 @@
 import simpy
 from CI import CIRep
 
+#----------------------------------------------------------------------------
+
+def create_ci_rep(activity, is_source, reqtr, respdr, if_name, tx_id):
+
+    ci_rep = CIRep(activity, is_source)
+    fd     = ci_rep.field_dict
+    fd["requestor"]   = reqtr
+    fd["responder"]   = respdr
+    fd["if-fnx-name"] = if_name
+    fd["tx-id"]       = tx_id
+    if (is_source):
+        fd["sent-at"]     = env.now
+    else:
+        fd["received-at"] = env.now
+
+    return ci_rep
+
+#----------------------------------------------------------------------------
+
 def segment_dispatcher(env, store):
+
     while True:
+
         tx_id = 100 + env.now
 
-        ci_rep = CIRep("request", True)
-        fd     = ci_rep.field_dict
-        fd["requestor"]   = "seg_dispatcher"
-        fd["responder"]   = "seg_receiver"
-        fd["if-fnx-name"] = "segrecv/publish"
-        fd["sent-at"]     = env.now
-        fd["tx-id"]       = tx_id
+        '''
+        ci_rep = create_ci_rep(
+                    "request",
+                    True,
+                    "segsend",
+                    "segrecv",
+                    "segrecv/publish",
+                    tx_id)
         print(ci_rep)
+        '''
 
         store.put(tx_id)
 
         yield env.timeout(2)
 
+#----------------------------------------------------------------------------
 
 def segment_receiver(env, store):
+
     while True:
+
         tx_id = yield store.get()
 
-        ci_rep = CIRep("request", False)
-        fd     = ci_rep.field_dict
-        fd["requestor"]   = "seg_dispatcher"
-        fd["responder"]   = "seg_receiver"
-        fd["if-fnx-name"] = "segrecv/publish"
-        fd["received-at"] = env.now
-        fd["tx-id"]       = tx_id
+        ci_rep = create_ci_rep(
+                    "request",
+                    False,
+                    "segsend",
+                    "segrecv",
+                    "segrecv/publish",
+                    tx_id)
         print(ci_rep)
 
         yield env.timeout(1)
 
+#----------------------------------------------------------------------------
 
 env = simpy.Environment()
 store = simpy.Store(env)
