8fe96713d4d4914c0d581d0fd472a6d982a3be4e
ksridhar <ksridhar@amagi.com>
2021-06-14 20:59:54 +0530

    first running version of trials/09/run.sh

--- trials/09/run.sh
+++ trials/09/run.sh
@@ -13,8 +13,16 @@ TMP2=`mktemp`
 
 DIRNAME=$(readlink -e $(dirname $0))
 
+#+----------------------+
+#| activate python venv |
+#+----------------------+
+
 source $TAL_VENV_ACTIVATE_FILE_PATH
 cd $DIRNAME
+
+#+---------------+
+#| generate stub |
+#+---------------+
 
 rm -vf helloworld_pb2.py helloworld_pb2_grpc.py
 
@@ -25,13 +33,27 @@ python \
     --grpc_python_out=. \
     helloworld.proto
 
+echo "generated rpc stub"
+
+#+--------------+
+#| start server |
+#+--------------+
+
 python greeter_server.py &
 SERVER_PID=$!
 disown #https://stackoverflow.com/a/23645819/1029379
 echo "started server at pid $SERVER_PID"
 sleep 2
 
+#+--------------+
+#| start client |
+#+--------------+
+
 python greeter_client.py
+
+#+-------------+
+#| stop server |
+#+-------------+
 
 sleep 2
 echo "stopping server at pid $SERVER_PID"
@@ -41,6 +63,10 @@ if ps -p $SERVER_PID 2>/dev/null
 then
     kill -9 $SERVER_PID
 fi
+
+#+------------------------+
+#| deactivate python venv |
+#+------------------------+
 
 set +u
 deactivate
