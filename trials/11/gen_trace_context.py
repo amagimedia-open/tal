9dce444d3d58a9a449294c2b02b863101c201261
ksridhar <ksridhar@amagi.com>
2021-06-19 00:07:14 +0530

    a more compressed and clarity encoding for trace_id in trace context

--- trials/11/gen_trace_context.py
+++ trials/11/gen_trace_context.py
@@ -35,7 +35,7 @@ def gen_default_id_dict (**kwargs):
 
 #----------------------------------------------------------------------------
 
-def str_2_hex(s):
+def str_2_int(s):
     l = bytearray(s.encode())
     s = "" 
     for i in l:
@@ -45,47 +45,116 @@ def str_2_hex(s):
 
 #----------------------------------------------------------------------------
 
+def hexencode_str(s):
+    l = bytearray(s.encode())
+    s = "" 
+    for i in l:
+        s = s + hex(i)[2:]
+
+    return s
+
+#----------------------------------------------------------------------------
+
 def id_dict_2_int_list (id_dict):
     l = [ 
           id_dict["gtc_ver"], 
           id_dict["ss_epoch"] 
         ]
-    l.append(str_2_hex(id_dict["comp_name"]))
-    l.append(str_2_hex(id_dict["comp_csver"]))
+    l.append(str_2_int(id_dict["comp_name"]))
+    l.append(id_dict["comp_csver"])
     return l
 
 #----------------------------------------------------------------------------
 
 def int_list_2_trace_id (int_list, salt):
-
     h = Hashids(salt, 32, "0123456789abcdef")
     e = h.encode(*int_list)
     return e
+
+#----------------------------------------------------------------------------
+
+def id_dict_2_int_list (id_dict):
+    l = [ 
+          id_dict["gtc_ver"], 
+          id_dict["ss_epoch"] 
+        ]
+    l.append(str_2_int(id_dict["comp_name"]))
+    l.append(str_2_int(id_dict["comp_csver"]))
+    return l
+
+#----------------------------------------------------------------------------
+
+def int_list_2_trace_id (int_list, salt):
+    h = Hashids(salt, 32, "0123456789abcdef")
+    e = h.encode(*int_list)
+    return e
+
+#----------------------------------------------------------------------------
+
+def id_dict_2_trace_id__1 (id_dict):
+
+    int_list = id_dict_2_int_list (id_dict)
+    print(int_list, file=sys.stderr)
+
+    return int_list_2_trace_id (int_list, "com.amagi.w3c.trace_context")
+
+#----------------------------------------------------------------------------
+
+def id_dict_2_trace_id__2 (id_dict):
+
+    hex_out_str = ""
+
+    gtc_ver_str = hex(id_dict["gtc_ver"])[2:]
+    print(f"gtc_ver_str = {gtc_ver_str}", file=sys.stderr)
+    hex_out_str += gtc_ver_str
+
+    ss_epoch_str = hex(id_dict["ss_epoch"])[2:]
+    print(f"ss_epoch_str = {ss_epoch_str}", file=sys.stderr)
+    hex_out_str += ss_epoch_str
+
+    comp_name_str = hexencode_str(id_dict["comp_name"])
+    print(f"comp_name_str = {comp_name_str}", file=sys.stderr)
+    hex_out_str += comp_name_str
+
+    comp_csver_str = id_dict["comp_csver"]
+    print(f"comp_csver_str = {comp_csver_str}", file=sys.stderr)
+    hex_out_str += comp_csver_str
+
+    return hex_out_str
 
 #-------------------------[MAIN]---------------------------------------------
 
 class UnitTests():
 
-    def tc_1(self):
-
-        id_dict = {
+    def __init__(self):
+        self.id_dict = {
             "gtc_ver"    : 9,           # max 1 digit
+            "ss_epoch"   : 2357899999,  # Max: Mon Sep 19 17:43:19 IST 2044
             "comp_name"  : "FOO",       # max 3 characters
-            "comp_csver" : "999999",    # MMmmpp (Major Minor Patch)
-            "ss_epoch"   : 2357899999   # Max: Mon Sep 19 17:43:19 IST 2044
+            "comp_csver" : "9999FE"     # MMmmpp (Major Minor Patch) in hex !
         }
-        print(id_dict, file=sys.stderr)
 
-        int_list = id_dict_2_int_list (id_dict)
-        print(int_list, file=sys.stderr)
+    def tc_1(self):
+        print(self.id_dict, file=sys.stderr)
+        trace_id_1 = id_dict_2_trace_id__1 (self.id_dict)
+        print(trace_id_1, len(trace_id_1), file=sys.stderr)
 
-        trace_id = int_list_2_trace_id (int_list, "com.amagi.w3c.trace_context")
-        print(trace_id, len(trace_id), file=sys.stderr)
+    def tc_2(self):
+        # $ printf "%d\n" 0xffffffff
+        # 4294967295
+        # $ date --date=@4294967295
+        # Sun Feb  7 11:58:15 IST 2106
+
+        print(self.id_dict, file=sys.stderr)
+        trace_id_2 = id_dict_2_trace_id__2 (self.id_dict)
+        print(trace_id_2, len(trace_id_2), file=sys.stderr)
 
 
 if __name__ == '__main__':
 
     ut = UnitTests()
     ut.tc_1()
+    print("#---", file=sys.stderr)
+    ut.tc_2()
 
 
