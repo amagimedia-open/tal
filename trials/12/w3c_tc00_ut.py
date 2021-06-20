13e19108613a681f6fd2784c05185d0d2abf2cad
ksridhar <ksridhar@amagi.com>
2021-06-20 20:46:15 +0530

    modified w3c_tc00_ut.py to take in user defined traceid and parentid generators

--- trials/12/w3c_tc00_ut.py
+++ trials/12/w3c_tc00_ut.py
@@ -3,6 +3,32 @@ import w3c_tc00_traceparent as TP
 
 #----------------------------------------------------------------------------
 
+class W3CTC00NaiveTraceIdGenerator():
+
+    def __init__(self):
+        self.counter = 0xdeadbeef
+        return
+
+    def generate(self):
+        s = hex(self.counter)[2:].zfill(32)
+        self.counter += 1
+        return s
+
+#----------------------------------------------------------------------------
+
+class W3CTC00NaiveParentIdGenerator():
+
+    def __init__(self):
+        self.counter = 0
+        return
+
+    def generate(self):
+        self.counter += 1
+        s = hex(self.counter)[2:].zfill(16)
+        return s
+
+#----------------------------------------------------------------------------
+
 class UnitTests():
 
     def __init__(self):
@@ -33,7 +59,9 @@ class UnitTests():
         self.root_n_forward("r-1-tx-1", tp_r_1)
         self.root_n_forward("r-1-tx-2", tp_r_1)
 
-        tp_r_2 = TP.W3CTC00TraceparentRoot()
+        tp_r_2 = TP.W3CTC00TraceparentRoot(
+                    trace_id_gen=W3CTC00NaiveTraceIdGenerator(),
+                    parent_id_gen=W3CTC00NaiveParentIdGenerator())
         self.root_n_forward("r-2-tx-1", tp_r_2)
         self.root_n_forward("r-2-tx-2", tp_r_2)
 
