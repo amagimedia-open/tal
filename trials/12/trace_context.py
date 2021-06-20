38d3415762942c3d3b4c2e115896db8af5989744
ksridhar <ksridhar@amagi.com>
2021-06-20 14:54:43 +0530

    modified tal/trials/12/trace_context.py

--- trials/12/trace_context.py
+++ trials/12/trace_context.py
@@ -8,19 +8,7 @@ import sys
 
 #----------------------------------------------------------------------------
 
-class TraceContextFnxs():
-
-    @staticmethod
-    def hexencode_str(s):
-        l = bytearray(s.encode())
-        s = "" 
-        for i in l:
-            s = s + hex(i)[2:]
-        return s
-
-#----------------------------------------------------------------------------
-
-class TraceIdGenerator():
+class W3CTC00TraceIdGenerator():
     """
     Default implementation for
     https://www.w3.org/TR/trace-context/#trace-id
@@ -34,7 +22,7 @@ class TraceIdGenerator():
 
 #----------------------------------------------------------------------------
 
-class ParentIdGenerator():
+class W3CTC00ParentIdGenerator():
     """
     Default implementation for
     https://www.w3.org/TR/trace-context/#parent-id
@@ -46,18 +34,105 @@ class ParentIdGenerator():
     def generate(self):
 
         rand_32 = random.randint(0, 0xffffffff)
-        s_r32   = hex(rand_32)[2:]
+        s_r32   = hex(rand_32)[2:].zfill(8)
 
         seconds_since_epoch = \
             int((datetime.datetime.utcnow() - \
                  datetime.datetime(1970, 1, 1)).total_seconds())
-        s_sse = hex(seconds_since_epoch)[2:]
+        s_sse = hex(seconds_since_epoch)[2:].zfill(8)
 
-        return f"{s_r32:8}{s_sse:8}"
+        return f"{s_r32}{s_sse}"
 
 #----------------------------------------------------------------------------
 
-class TraceContext00Generator():
+class W3CTC00Traceparent():
+
+    def __init__(self, trace_id_str, parent_id_str, trace_flags_str):
+        self.m_version_str     = "00"
+        self.m_trace_id_str    = trace_id_str
+        self.m_parent_id_str   = parent_id_str
+        self.m_trace_flags_str = trace_flags_str
+
+        self.traceparent = f"{self.m_version_str}-" \
+                           f"{self.m_trace_id_str}-" \
+                           f"{self.m_parent_id_str}-" \
+                           f"{self.m_trace_flags_str}" 
+
+    def __str__(self):
+        return self.traceparent
+
+    @property
+    def version_str(self):
+        return self.m_version_str
+
+    @property
+    def trace_id_str(self):
+        return self.m_trace_id_str
+
+    @property
+    def parent_id_str(self):
+        return self.m_parent_id_str
+
+    @property
+    def flags_str(self):
+        return self.m_trace_flags_str
+
+    @staticmethod
+    def parse (tc00_str):
+        """
+        Returns a 3-tuple
+        (parse_status, parse_error_str, W3CTC00Traceparent)
+        """
+
+        while True: # just need a 'block'
+
+            fields = tc00_str.split("-")
+            n = len(fields)
+            if (n != 4):
+                error_str =  "w3c/tc/00/tp num. fields mismatch. " +\
+                            f"found {n}. expected 4."
+                ret = (False, error_str, None)
+                break
+
+            version_str = fields[0]
+            if (version_str != "00"): 
+                error_str =  "w3c/tc/00/tp 'version' mismatch. " +\
+                            f"found {version_str}. expected 00."
+                ret = (False, error_str, None)
+                break
+
+            trace_id_str = fields[1]
+            if (trace_id_str == "0" * 32): 
+                error_str = "w3c/tc/00/tp invalid 'trace id' value of all 0's."
+                ret = (False, error_str, None)
+                break
+
+            parent_id_str = fields[2]
+            if (parent_id_str == "0" * 32): 
+                error_str = "w3c/tc/00/tp invalid 'parent id' value of all 0's."
+                ret = (False, error_str, None)
+                break
+
+            trace_flags_str = fields[3]
+            if (not (trace_flags_str == "00" or 
+                     trace_flags_str == "01")):
+                # SAMPLED_FLAG
+                error_str = "w3c/tc/00/tp invalid 'trace flag' value."
+                ret = (False, error_str, None)
+                break
+
+            traceparent = W3CTC00Traceparent(
+                            trace_id_str,
+                            parent_id_str,
+                            trace_flags_str)
+            ret = (True, "", traceparent)
+            break
+
+        return ret
+
+#----------------------------------------------------------------------------
+
+class W3CTC00TraceparentCreator():
     """
     Default implementation for
     https://www.w3.org/TR/trace-context version 00
@@ -74,25 +149,64 @@ class TraceContext00Generator():
         if "trace_id_gen" in kwargs:
             self.trace_id_gen = kwargs["trace_id_gen"]
         else:
-            self.trace_id_gen = TraceIdGenerator()
+            self.trace_id_gen = W3CTC00TraceIdGenerator()
 
         if "parent_id_gen" in kwargs:
             self.parent_id_gen = kwargs["parent_id_gen"]
         else:
-            self.parent_id_gen = ParentIdGenerator()
+            self.parent_id_gen = W3CTC00ParentIdGenerator()
 
         self.trace_id = self.trace_id_gen.generate()
 
         return
 
-    def generate(self):
+    def generate(self, sampled_flag=True):
+        """
+        returns an W3CTC00Traceparent instance
+        """
 
-        version     = "00"
-        trace_id    = self.trace_id
-        parent_id   = self.parent_id_gen.generate()
-        trace_flags = "01"  # SAMPLED FLAG
+        return W3CTC00Traceparent(
+                self.trace_id,
+                self.parent_id_gen.generate(),
+                "01" if (sampled_flag) else "00")
+        
+#----------------------------------------------------------------------------
 
-        return f"{version}-{trace_id}-{parent_id}-{trace_flags}"
+class W3CTC00TraceparentForwarder():
+    """
+    Default implementation for
+    https://www.w3.org/TR/trace-context version 00
+    TODO: use incoming trace context
+          bad incoming trace context
+    """
+
+    def __init__(self, tcp, **kwargs):
+        """
+        tcp => Non erroneous W3CTC00TraceparentParser
+        """
+
+        # kwargs parameter names
+        # parent_id_gen
+
+        if "parent_id_gen" in kwargs:
+            self.parent_id_gen = kwargs["parent_id_gen"]
+        else:
+            self.parent_id_gen = W3CTC00ParentIdGenerator()
+
+        self.tcp = tcp
+
+        return
+
+    def generate(self, sampled_flag=True):
+
+        """
+        returns an W3CTC00Traceparent instance
+        """
+
+        return W3CTC00Traceparent(
+                self.tcp.trace_id_str,
+                self.parent_id_gen.generate(),
+                "01" if (sampled_flag) else self.tcp.trace_flags_str)
 
 #----------------------------------------------------------------------------
 
@@ -101,16 +215,32 @@ class UnitTests():
     def __init__(self):
         return
 
+    def generate_and_forward(self, tp_c):
+
+        tp_0 = tp_c.generate()
+        print(tp_0, file=sys.stderr)
+
+        (status, error_str, tp_p_0) = W3CTC00Traceparent.parse(str(tp_0))
+        if (not status):
+            print (error_str, file=sys.stderr)
+        else:
+            tp_f_0 = W3CTC00TraceparentForwarder(tp_p_0)
+            tp_00 = tp_f_0.generate()
+            print(tp_00, file=sys.stderr)
+
     def tc_1(self):
-        tc00_g = TraceContext00Generator()
-        tc00_1 = tc00_g.generate()
-        tc00_2 = tc00_g.generate()
-        print(tc00_1, len(tc00_1), file=sys.stderr)
-        print(tc00_2, len(tc00_2), file=sys.stderr)
+        tp_c = W3CTC00TraceparentCreator()
+        print("#---[gnf]---")
+        self.generate_and_forward(tp_c)
+        print("#---[gnf]---")
+        self.generate_and_forward(tp_c)
 
 
 if __name__ == '__main__':
 
     ut = UnitTests()
     ut.tc_1()
+
+
+    
 
